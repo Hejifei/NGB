d98d2b967d124cab2d298ef89f645c30c3727d19
hejifei <jifei.he@goodwe.com>
2023-03-18 15:40:01 +0800

    图片截取 wip

--- miniprogram/pages/index/img_upload/img_upload.ts
+++ miniprogram/pages/index/img_upload/img_upload.ts
@@ -4,10 +4,43 @@ import WeCropper from '@we-cropper/index.js'
 
 Page({
   data: {
+    src:'',
+    width: 128,//宽度
+    height: 128,//高度
   },
-  onShow() {
-        
+  onLoad: function (options) {
+    //获取到image-cropper实例
+        this.cropper = this.selectComponent("#image-cropper");
+        //开始裁剪
+        // this.setData({
+        //     src:"../../../assets/imgs/demo2.jpg",
+        // });
+        // wx.showLoading({
+        //     title: '加载中'
+        // })
     },
-    confirmUpload() {
-    }
+    cropperload(e){
+        console.log("cropper初始化完成");
+    },
+    loadimage(e){
+        console.log("图片加载完成",e.detail);
+        // wx.hideLoading();
+        //重置图片角度、缩放、位置
+        this.cropper.imgReset();
+    },
+    clickcut(e) {
+        console.log({
+            detail: e.detail
+        });
+        //点击裁剪框阅览图片
+        wx.previewImage({
+            current: e.detail.url, // 当前显示图片的http链接
+            urls: [e.detail.url] // 需要预览的图片http链接列表
+        })
+    },
+//   onShow() {
+        
+//     },
+//     confirmUpload() {
+//     }
 })
